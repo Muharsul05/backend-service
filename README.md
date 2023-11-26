283c9733b38b73d337ec270c54bbfdf1c4f6f002
Magarusik <mrasul13112001@gmail.com>
2023-11-26 10:02:59 +0300

    conf: update application.yml pom.xml add: Liquibase migrations update: docker .env files and README.md

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 * JDK 21, если нет то надо скачать и установить: [JDK 21](https://download.oracle.com/java/21/latest/jdk-21_windows-x64_bin.exe).
 * PostgreSQL любая версия: [PostgreSQL](https://sbp.enterprisedb.com/getfile.jsp?fileid=1258792).
- 
+
 # Проверка установки:
     Для проверки что JDK установлена: java --version
     Для проверки что установлен PostgreSQL: В пуске должна быть программа PgAdmin
 
-# Запуск проекта:
+## Запуск проекта:
     git clone https://github.com/Muharsul05/backend-service
     cd backend-service
     ./mwnv spring-boot:run 
@@ -17,4 +17,5 @@
     Комманды для развёртки проекта:
     cd docker
     docker compose up -d
-    
+
+### На проекте есть документация доступная по url: [Документация](http://localhost:8080/microservice/api/docs-ui.html)
--- src/main/java/ru/magarusik/microservice/entity/PostEntity.java
+++ src/main/java/ru/magarusik/microservice/entity/PostEntity.java
@@ -2,15 +2,12 @@ package ru.magarusik.microservice.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.Date;
 
-@Entity
+@Entity(name = "post_entity")
 @Data
 @Getter
 @Setter
@@ -19,10 +16,19 @@ import java.util.Date;
 public class PostEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
-    @JsonFormat(pattern="dd.MM.yyyy")
+
+    @JsonFormat(pattern = "dd.MM.yyyy")
+    @Column(name = "date")
     private Date date;
+
+    @Column(name = "full_text")
     private String fullText;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "type")
     private String type;
 }
